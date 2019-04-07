de3cf18035273ad2c77efa02657863799dd0602b
SeongMin Han <hsm950825@naver.com>
2019-04-07 13:14:41 +0900

    141~160 Question

--- 141-160/157.search with Divide and Conquer.js
+++ 141-160/157.search with Divide and Conquer.js
@@ -23,3 +23,23 @@
 
 // var arr = [1, 3, 16, 22, 31, 33, 34]
 // console.log(search(arr, 31)); --> 4
+
+function search(array, value) {
+  var start = 0;
+  var end = array.length - 1;
+
+  while (start <= end) {
+    var mid = Math.floor((start + end + 1) / 2);
+    if (array[mid] === value) {
+      return mid;
+    } else if (array[mid] < value) {
+      start = mid + 1;
+    } else if (array[mid] > value) {
+      end = mid - 1;
+    }
+    console.log("mid is,", mid, "and value is, ", value);
+  }
+  return null;
+}
+
+search([1, 3, 16, 27, 35, 61, 66, 68, 72, 77, 80], 60);
